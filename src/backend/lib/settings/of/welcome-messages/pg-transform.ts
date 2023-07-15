1b9c0037e687f8e4802dd6e4fb179cebe5bd7bfc
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-07-15 10:08:14 -0500

    added welcome messages

---
+++ src/backend/lib/settings/of/welcome-messages/pg-transform.ts
@@ -0,0 +1,31 @@
+import { OFWelcomeMessage, PGOFWelcomeMessage } from "./types.js";
+
+export const transformPGOFWelcomeMessage = (pgMessage: PGOFWelcomeMessage) => {
+  const message: OFWelcomeMessage = {
+    id: pgMessage.id,
+    siteUserId: pgMessage.site_user_id,
+    userId: pgMessage.user_id,
+    createdAt: pgMessage.created_at.getTime(),
+    updatedAt: pgMessage.updated_at.getTime(),
+    message: {
+      image: pgMessage.image,
+      script: pgMessage.script,
+      price: pgMessage.price,
+    },
+  };
+  return message;
+};
+
+export const transformOFWelcomeMessage = (params: OFWelcomeMessage) => {
+  const pgMessage: PGOFWelcomeMessage = {
+    id: params.id,
+    site_user_id: params.siteUserId,
+    user_id: params.userId,
+    created_at: new Date(params.createdAt),
+    updated_at: new Date(params.updatedAt),
+    image: params.message.image,
+    script: params.message.script,
+    price: params.message.price,
+  };
+  return pgMessage;
+};
