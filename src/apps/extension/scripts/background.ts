814180d7fbd095b7f1176706bfb8471903208e1d
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-08-03 15:55:29 -0500

    wait until user-id header is ready to save auth

--- src/apps/extension/scripts/background.ts
+++ src/apps/extension/scripts/background.ts
@@ -10,50 +10,43 @@ registerMessageHandler(context);
  * attempt to update auth when a request is made
  */
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  function (details) {
+  function(details) {
     const requestHeaders = details.requestHeaders ?? [];
     const url = details.url;
-
+    let xbc = '';
+    let authId = '';
+    let sess = "";
     for (const requestHeader of requestHeaders) {
       if (requestHeader.name === "x-bc") {
-        const xbc = requestHeader.value;
-        if (xbc) {
-          chrome.cookies.getAll({ url }, function (cookies) {
-            let authId = "";
-            let sess = "";
-            for (const cookie of cookies) {
-              switch (cookie.name) {
-                case "auth_id": {
-                  authId = cookie.value;
-                  break;
-                }
-                case "sess": {
-                  sess = cookie.value;
-                  break;
-                }
-              }
-            }
-
-            if (!xbc && !sess && !authId) {
-              return;
-            }
-
-            const auth: Auth = {
-              xbc,
-              authId,
-              sess,
-            };
-            context.isReady
-              .then(() => {
-                return saveAuth(auth, context);
-              })
-              .catch((err) => {
-                console.error(`Failed to save auth for ${xbc}`, err);
-              });
-          });
-        }
-        return;
+        xbc = requestHeader.value ?? '';
+      } else if (requestHeader.name === 'user-id') {
+        authId = requestHeader.value ?? '';
       }
+    }
+
+    if (xbc && authId) {
+      chrome.cookies.getAll({ url }, function(cookies) {
+        for (const cookie of cookies) {
+          if (cookie.name === 'sess') {
+            sess = cookie.value;
+          }
+        }
+
+        if (xbc && sess && authId) {
+          const auth: Auth = {
+            xbc,
+            authId,
+            sess,
+          };
+          context.isReady
+            .then(() => {
+              return saveAuth(auth, context);
+            })
+            .catch((err) => {
+              console.error(`Failed to save auth for ${xbc}`, err);
+            });
+        }
+      });
     }
   },
   { urls: ["https://onlyfans.com/*"] },
