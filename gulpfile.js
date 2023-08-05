fa91549aa6cfa69667fe0a2cf79d599e53532125
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-08-05 12:24:39 -0500

    include zip in build process

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+
+exports.default = () => (
+  gulp.src('ext-dist/*')
+    .pipe(zip('only-agent.zip'))
+    .pipe(gulp.dest('./'))
+);
