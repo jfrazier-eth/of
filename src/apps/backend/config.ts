d6616a20941ea92311a385b01cc10dedc55c46a3
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-08-09 20:34:47 -0500

    switching to render

--- src/apps/backend/config.ts
+++ src/apps/backend/config.ts
@@ -5,6 +5,15 @@ import prodServiceAccount from "../../creds/firebase-prod.json";
 
 if (process.env.DEPLOY_ENV === "prod") {
   loadEnv({ path: ".env.production", override: true });
+
+  switch(process.env.OF_APP) {
+    case 'QUEUES':
+      loadEnv({ path: ".env.queues", override: true });
+      break;
+    case 'SERVER':
+      loadEnv({ path: ".env.server", override: true });
+      break;
+  }
   console.log(`Loaded prod env`);
 }
 
